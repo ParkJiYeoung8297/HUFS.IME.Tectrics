673c299c04f2aa3f9cbbb6042fbeb55b4a176c04
ParkJiYeoung8297 <wldud8297@gmail.com>
2024-03-13 22:45:02 +0900

    다중 경로 100 api 불러오기

--- Tectrics/Tectrics/views.py
+++ Tectrics/Tectrics/views.py
@@ -4,5 +4,5 @@ from rest_framework.views import APIView
 
 class Main(APIView):
     def get(self,request):
-        kakao_key = open("kakaoRestKey.txt", "r").read()
-        return render(request,"Tectrics/main.html",{'kakao_key': kakao_key})
+        tmap_key = open("TmapRestKey.txt", "r").read()
+        return render(request,"Tectrics/main.html",{'tmap_key': tmap_key})
