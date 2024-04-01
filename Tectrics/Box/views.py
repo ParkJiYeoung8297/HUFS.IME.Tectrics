10672583631498989065e417741ec52821d7ffdc
ParkJiYeoung8297 <wldud8297@gmail.com>
2024-04-01 18:17:46 +0900

    DB에서 가져온 데이터로 마커 띄우기

--- Tectrics/Box/views.py
+++ Tectrics/Box/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from Order.models import Order
 
 class BoxList(APIView):
     def get(self,request):
-        return render(request,"Box/boxlist.html")
+        order_list=Order.objects.values("box_code","name","road_address","detail_address","phone")
+        context={"order_list":order_list}
+        return render(request,"Box/boxlist.html",context)
     
--- Tectrics/Route/views.py
+++ Tectrics/Route/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from Order.models import Order
+from Order.models import BoxData
 
 class Road(APIView):
     def get(self,request):
@@ -11,8 +12,10 @@ class Road(APIView):
 class Map(APIView):
     def get(self,request):
         tmap_key = open("TmapRestKey.txt", "r").read()
-        address_list = Order.objects.values("road_address", "detail_address")
-        context = {"address_list": address_list,'tmap_key': tmap_key}
+        order_list = Order.objects.values("box_code","road_address", "detail_address")
+        address_list=BoxData.objects.values("latitude","longitude","latitude2","longitude2")
+        context = {"order_list":order_list,"address_list": address_list,'tmap_key': tmap_key}
         return render(request,"Route/map.html",context)
-
+     
+    
 
